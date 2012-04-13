052fc34acbf7ada2be1eb87777206143e45151dd
Andris Reinman <andris@node.ee>
2012-04-13 10:27:05 +0300

    Fixes #8 - filter carriage returns before highlighting

--- examples/test.js
+++ examples/test.js
@@ -1,11 +1,11 @@
 
 
 var hl = require("../lib/highlight.js").Highlight,
-    code_string = "<?php\n"+
-                  "\techo \"Hello world!\";\n"+
-                  "\tfor($i=0;$i<100;$i++){\n"+
-                  "\t\techo \"$i\";\n"+
-                  "\t}\n"+
+    code_string = "<?php\r\n"+
+                  "\techo \"Hello world!\";\r\n"+
+                  "\tfor($i=0;$i<100;$i++){\r\n"+
+                  "\t\techo \"$i\";\r\n"+
+                  "\t}\r\n"+
                   "?>",
 
     code_block =  "<p>PHP code:</p>\n"+
